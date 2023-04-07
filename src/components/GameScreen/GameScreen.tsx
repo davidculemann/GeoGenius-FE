25c6d0b1f1017c2f9797ed119fe2980a41170198
davidculemann <davidculemann@gmail.com>
2023-04-08 00:28:14 +0400

    add fetching country data

--- src/components/GameScreen/GameScreen.tsx
+++ src/components/GameScreen/GameScreen.tsx
@@ -1,7 +1,18 @@
 import { useParams } from "react-router-dom";
+import { useEffect } from "react";
 
 function GameScreen() {
 	const { mode } = useParams();
+
+	const getCountryData = async () => {
+		const response = await fetch("https://restcountries.eu/rest/v2/all");
+		const data = await response.json();
+		console.log(data);
+	};
+
+	useEffect(() => {
+		getCountryData();
+	}, []);
 
 	return <div className="game-screen">{mode}</div>;
 }
