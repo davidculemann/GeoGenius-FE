f1fc84a096066f205ebfe44f519e4e9e6eab5fde
davidculemann <davidculemann@gmail.com>
2023-04-08 00:34:37 +0400

    add country svgs

--- src/components/GameScreen/GameScreen.tsx
+++ src/components/GameScreen/GameScreen.tsx
@@ -1,12 +1,15 @@
 import { useParams } from "react-router-dom";
 import { useEffect } from "react";
+import axios from "axios";
 
 function GameScreen() {
 	const { mode } = useParams();
 
 	const getCountryData = async () => {
-		const response = await fetch("https://restcountries.eu/rest/v2/all");
-		const data = await response.json();
+		const response = await axios.get(
+			"https://restcountries.com/v3.1/all?fields=name,area,population"
+		);
+		const data = await response.data;
 		console.log(data);
 	};
 
