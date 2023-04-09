31f8af4cfa3fbacdea144e000a93c5a7aa319457
davidculemann <davidculemann@gmail.com>
2023-04-09 22:31:09 +0100

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base:
-		process.env.NODE_ENV === "production"
-			? "https://geogenius.vercel.app/dist/"
-			: "/",
+	base: "./",
 });
